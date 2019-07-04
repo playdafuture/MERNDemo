0199190b11faed737b587e2bb687c402329ffa5b
Jinqiu Liu <playdafuture@gmail.com>
2019-07-04 13:04:32 -0400

    local build 0

---
+++ client/build/precache-manifest.6f60982b9684c454ce3f91e882ddd1ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ef1421d49ae07ce2baa3bd25bad947c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ccdbf8411875a8e081e",
+    "url": "/static/css/main.bc587b98.chunk.css"
+  },
+  {
+    "revision": "342a93db7742506c75f4",
+    "url": "/static/js/2.193c6a31.chunk.js"
+  },
+  {
+    "revision": "5ccdbf8411875a8e081e",
+    "url": "/static/js/main.4a03cb88.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a031bdc78b3e946b454f705d609e8143",
+    "url": "/static/media/family-background.a031bdc7.jpg"
+  }
+]);
